cd8066aabef0df9291620cd10f9c7a2845942cc8
Clarence Montenegro <cmontenegro@tribalworldwide.gt>
2022-09-12 08:40:00 -0600

    Add tests to the presenter UsersListPresenterTest

--- UsersListTests/Features/MainList/Modules/UsersList/User Interface/Presenter/UsersListPresenterTest.swift
+++ UsersListTests/Features/MainList/Modules/UsersList/User Interface/Presenter/UsersListPresenterTest.swift
@@ -13,13 +13,61 @@ import Quick
 final class UsersListPresenterTest: QuickSpec {
 
     override func spec() {
+        
+        var presenter: UsersListPresenter!
+        var view: MockView!
+        var interactor: MockInteractor!
+        var router: MockRouter!
+        
         beforeEach {
+            presenter = UsersListPresenter()
+            view = MockView()
+            interactor = MockInteractor()
+            router = MockRouter()
+            
+            presenter.view = view
+            presenter.interactor = interactor
+            presenter.router = router
         }
 
         afterEach {
+            presenter = nil
+            view = nil
+            interactor = nil
+            router = nil
         }
 
         describe("A UsersList Presenter") {
+            
+            it("View is ready") {
+                presenter.viewIsReady()
+                
+                expect(view.showLoadingIndicatorCount).to(equal(1))
+                expect(view.setUpInitialStateCount).to(equal(1))
+                expect(interactor.retrieveUsersCount).to(equal(1))
+            }
+            
+            it("Should route to detail") {
+                let user = UserInfoElement(id: 1, name: "Test", phone: "89898", email: "test@gmail.com")
+                presenter.routeToDetail(user: user)
+                
+                expect(router.routeToDetailCount).to(equal(1))
+            }
+            
+            it("Should call didRetrieveUsers") {
+                let user = UserInfoElement(id: 1, name: "Test", phone: "89898", email: "test@gmail.com")
+                presenter.didRetrieveUsers(users: [user])
+                
+                expect(view.updateUsersListCount).to(equal(1))
+                expect(view.hideLoadingIndicatorCount).to(equal(1))
+            }
+            
+            it("Should call didFailRetrievingUsers") {
+                presenter.didFailRetrievingUsers(title: "Error", message: "Error retrieving")
+                
+                expect(view.showErrorCount).to(equal(1))
+                expect(view.hideLoadingIndicatorCount).to(equal(1))
+            }
         }
     }
 
