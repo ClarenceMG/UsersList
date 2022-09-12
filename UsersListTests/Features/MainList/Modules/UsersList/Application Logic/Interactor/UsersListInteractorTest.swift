4705a1875e0fd4de1d987253b647a506eb27ebc7
Clarence Montenegro <cmontenegro@tribalworldwide.gt>
2022-09-12 17:55:22 -0600

    Add tests fot the interactor of users list

--- UsersListTests/Features/MainList/Modules/UsersList/Application Logic/Interactor/UsersListInteractorTest.swift
+++ UsersListTests/Features/MainList/Modules/UsersList/Application Logic/Interactor/UsersListInteractorTest.swift
@@ -7,6 +7,7 @@
 
 import Nimble
 import Quick
+import RealmSwift
 
 @testable import UsersList
 
@@ -17,25 +18,67 @@ final class UsersListInteractorTest: QuickSpec {
         var interactor: UsersListInteractor!
         var presenter: MockPresenter!
         var mockAPIService: APIServiceMock!
+        var localRealm: Realm!
         
         beforeEach {
             interactor = UsersListInteractor()
             presenter = MockPresenter()
             mockAPIService = APIServiceMock()
+            localRealm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "users"))
             
             interactor.output = presenter
             interactor.apiService = mockAPIService
+            interactor.localRealm = localRealm
         }
 
         afterEach {
             presenter = nil
             interactor = nil
             mockAPIService = nil
+            localRealm = nil
         }
 
         describe("A UsersList Interactor") {
             
+            it("Should retrieve users from services") {
 
+                interactor.retrieveUsers()
+                
+                expect(presenter.didRetrieveUsersCount).to(equal(1))
+                expect(presenter.users).notTo(beNil())
+                expect(localRealm.objects(UserInformation.self).count).to(beGreaterThan(0))
+            }
+            
+            it("Should retrieve users from local storage") {
+
+                let userInformation = UserInformation()
+                userInformation.id = 1
+                userInformation.name = "Carlos Diaz"
+                userInformation.phone = "898989"
+                userInformation.email = "carlos@gmail.com"
+                
+                try! localRealm.write {
+                    localRealm.add(userInformation)
+                }
+                
+                interactor.retrieveUsers()
+                
+                expect(presenter.didRetrieveUsersCount).to(equal(1))
+                expect(presenter.users).notTo(beNil())
+                expect(localRealm.objects(UserInformation.self).count).to(beGreaterThan(0))
+            }
+            
+            it("Should fail retrieving users from services") {
+
+                let expectedMessage = "Hubo un problema obteniendo la lista de usuarios"
+                mockAPIService.kindOfTest = .failure
+                interactor.retrieveUsers()
+
+                expect(presenter.didFailRetrievingUsersCount).to(equal(1))
+                expect(localRealm.objects(UserInformation.self).count).to(equal(0))
+                expect(presenter.title).to(equal(MainList.Common.error))
+                expect(presenter.message).to(equal(expectedMessage))
+            }
         }
     }
 
